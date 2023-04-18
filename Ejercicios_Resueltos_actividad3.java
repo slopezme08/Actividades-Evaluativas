e204e9d8fb83e2b098e113f519a9b5729e001c72
slopezme08 <126433288+slopezme08@users.noreply.github.com>
2023-04-17 19:56:03 -0500

    Add files via upload

---
+++ Ejercicios_Resueltos_actividad3.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios_resueltos_actividad3;
+
+public class Ejercicios_Resueltos_actividad3 {
+
+    public static void main(String[] args) {
+        Interfaz_Actividad3 pestaña = new Interfaz_Actividad3();
+        pestaña.setVisible(true);
+    }
+    
+}
